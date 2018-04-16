cf8cab93405c891c96203a08e500082908bc7e87
Raj Kumar <rajkumar.cind@gmail.com>
2018-04-16 17:41:38 +0530

    deploy

--- app.js
+++ app.js
@@ -7,16 +7,9 @@ var express    = require("express"),
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/ttt-se");
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.bodyParser());
 app.use(bodyParser.json());
-
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
-
-
-
-
 
 
 app.get("/", function(req, res){
@@ -29,17 +22,11 @@ app.post("/submit", function(req, res){
 	fetch("http://terriblytinytales.com/test.txt").then(function(res){
 		return res.text();
 	}).then(function(body){
-		//console.log(body);
-		//res.send(body);
 		var wordCount = {};
 		var words = body.split(/\s/);
-		//res.send(words);
 		for(var i = 0;i < words.length;i++)
 			wordCount[words[i].toLowerCase()] = (wordCount[words[i].toLowerCase()] || 0) + 1;
-		
 		keysSorted = Object.keys(wordCount).sort(function(a, b){return wordCount[b] - wordCount[a]});
-
-		//res.send(keysSorted);
 		for(var i = 0;i < N;i++){
 			console.log(keysSorted[i]);
 		}
@@ -49,6 +36,16 @@ app.post("/submit", function(req, res){
 	});
 });
 
-app.listen(3380, function(){
-    console.log("The Server Has Started at 3380");
+//========================//
+// Dev Port Number//
+//========================//
+// app.listen(3380, function(){
+//     console.log("The Server Has Started at 3380");
+// });
+
+//========================//
+// Production Port Number//
+//========================//
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The Server Has Started");
 });
